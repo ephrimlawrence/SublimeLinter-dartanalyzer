cfa6546369f04a874243f7ce731a504f859b33ff
Lawrence Ephrim <ephrimlawrence@gmail.com>
2019-11-13 18:13:09 +0000

    Code Formatting

--- linter.py
+++ linter.py
@@ -2,21 +2,19 @@ from SublimeLinter.lint import Linter
 import logging
 
 
-logger = logging.getLogger('SublimeLinter.plugins.dartanalyzer')
+logger = logging.getLogger("SublimeLinter.plugins.dartanalyzer")
 
 
 class DartAnalyzer(Linter):
 
-    cmd = ('dartanalyzer', '${args}',  '.')
+    cmd = ("dartanalyzer", "${args}", ".")
     regex = (
-            r"(^Analyzing.*)|\s+"
-            r"((?P<warning>[lh]int)|(?P<error>error))"
-            r"\s.\s(?P<message>.*)"
-            r":(?P<line>\d{1,})"
-            r":(?P<col>\d{1})"
-        )
+        r"(^Analyzing.*)|\s+"
+        r"((?P<warning>[lh]int)|(?P<error>error))"
+        r"\s.\s(?P<message>.*)"
+        r":(?P<line>\d{1,})"
+        r":(?P<col>\d{1})"
+    )
 
     multiline = False
-    defaults = {
-        'selector': 'source.dart'
-    }
+    defaults = {"selector": "source.dart"}
